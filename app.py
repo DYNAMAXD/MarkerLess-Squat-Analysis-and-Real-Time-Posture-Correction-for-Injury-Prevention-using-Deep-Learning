eae601086ada7cef2e5257cb7ef4c195303d7cbc
Divyansh Yadav <DYNAMAXD@users.noreply.huggingface.co>
2026-08-13 07:18:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ with gr.Blocks(title="AlphaPose + MotionBERT Keypoint Extraction") as demo:
     )
 
 if __name__ == "__main__":
-    demo.queue().launch(show_api=False, quiet=False , share=True)
+    # demo.queue().launch(show_api=False, quiet=False , share=True)
+    demo.queue().launch(show_api=False)
